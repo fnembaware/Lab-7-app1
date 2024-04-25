26781e50ac6297f29688885f02f9341ae4bb75f9
fnembaware <fadzai@tlu.ee>
2024-04-25 20:57:11 +0300

    React API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import APIKey from './config';
+import Movie from './Components/Movie';
+
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
 
 function App() {
+  const [movieResults,setMovieResults] = useState([]);
+  //const movieResults = ['age',4512,true,'aa'];
+  
+  useEffect(()=> {
+    fetch(APIURL + APIKey)
+    .then((resp) => resp.json())
+    .then((data) => 
+    console.log(data));
+    setMovieResults(data.results);
+
+  }
+,[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {movieResults.map((element,index) => (<Movie key={index}/>)
+    
+  )}
+    </>
   );
 }
 
---
+++ src/Components/Movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Movie ()
+{
+    return(
+        <div>This is a movie component</div>
+        
+    );
+}
+
+export default Movie
