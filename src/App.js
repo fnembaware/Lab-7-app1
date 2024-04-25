f676306356b777f7717ec89c11b39ebf93c99031
fnembaware <fadzai@tlu.ee>
2024-04-25 21:33:28 +0300

    Passed the element object as prop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState,useEffect} from 'react';
+import{useState,useEffect} from 'react';
 import './App.css';
 import APIKey from './config';
 import Movie from './Components/Movie';
@@ -8,19 +8,19 @@ const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 function App() {
   const [movieResults,setMovieResults] = useState([]);
   //const movieResults = ['age',4512,true,'aa'];
-  
   useEffect(()=> {
     fetch(APIURL + APIKey)
     .then((resp) => resp.json())
-    .then((data) => 
-    console.log(data));
+    .then((data) => {
     setMovieResults(data.results);
 
-  }
-,[])
+  });
+},[]);
+
   return (
     <>
-    {movieResults.map((element,index) => (<Movie key={index}/>)
+    {movieResults.map((element,index) => (
+    <Movie key={index} movie={element}/>)
     
   )}
     </>
